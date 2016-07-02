2f4b1747a4033ef6450b25ecb21f4d6cf5a760bd
hcynnn216 <yuhongchao@gmail.com>
2016-07-01 20:17:05 -0700

    Wrote schema.sql and added chatter-box client

--- server/schema.sql
+++ server/schema.sql
@@ -1,13 +1,40 @@
+DROP DATABASE IF EXISTS chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
+DROP TABLE IF EXISTS messages;
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
+
+  id INTEGER(3) UNSIGNED AUTO_INCREMENT,
+  text VARCHAR(256),
+  user_id INTEGER(3),
+  room_id INTEGER(3),
+  PRIMARY KEY (id)
 );
 
 /* Create other tables and define schemas for them here! */
 
+DROP TABLE IF EXISTS users;
+    
+CREATE TABLE users (
+  id INTEGER AUTO_INCREMENT,
+  username VARCHAR(30),
+  PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS rooms;
+    
+CREATE TABLE rooms (
+  id INTEGER AUTO_INCREMENT,
+  roomname VARCHAR(30),
+  PRIMARY KEY (id)
+);
+
+ALTER TABLE messages ADD FOREIGN KEY (user_id) REFERENCES users (id);
+ALTER TABLE messages ADD FOREIGN KEY (room_id) REFERENCES rooms (id);
 
 
 
